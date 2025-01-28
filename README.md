5e9fe9c5c946d619ac14317b4545b3ebc5bcb491
NotLiu <aliu@lediur.com>
2025-01-27 19:17:54 -0600

    refactor about page to functional react, restyle navbar css color, dynamically show hide nav bar on scroll

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+NOTE FOR SELF:: NPM v18.19.0!
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
+import React, { Component, useEffect, useState, useRef } from "react";
 import { BrowserRouter as Router, Redirect, Route, Routes, Link } from "react-router-dom";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -10,70 +11,69 @@ import Gallery from "./pages/Gallery";
 import Inktober from "./pages/Inktober";
 import Work from "./pages/Work";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //xstate : xvalue
-    };
-  }
-  render() {
-    return (
-      <Router>
-        <div>
-          <div id="nav-bar">
-            <div id="nav-name">
-              <a href="/">ANDREW LIU</a>
-              <a href="mailto:aliu@lediur.com" id="email">
-                <i class="fas fa-envelope"></i>
-              </a>
-            </div>
-            <ul id="nav">
-              <li>
+function App() {
+  const [showNav, setShowNav] = useState(true);
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    setShowNav(prevPos.y < currPos.y);
+  });
+
+  return (
+    <Router>
+      <div>
+        <div id="nav-bar" className={showNav ? "show" : "hidden"}>
+          <div id="nav-name">
+            <a href="/">ANDREW LIU</a>
+            <a href="mailto:aliu@lediur.com" id="email">
+              <i class="fas fa-envelope"></i>
+            </a>
+          </div>
+          <ul id="nav">
+            {/* <li>
                 <Link to="/work" id="work">
                   WORK
                 </Link>
-              </li>
-              <li>
-                <Link to="/" id="about-but">
-                  ABOUT
-                </Link>
-              </li>
-              {/* <li>
+              </li> */}
+            <li>
+              <Link to="/" id="about-but">
+                ABOUT
+              </Link>
+            </li>
+            {/* <li>
                       <a href="#gallery" id="gallery">GALLERY</a>
                   </li> */}
-              <li>
-                <Link to="/projects" id="projects">
-                  PROJECTS
-                </Link>
-              </li>
-              <li>
-                <Link to="/gallery" id="gallery">
-                  GALLERY
-                </Link>
-              </li>
-              <li>
-                <a href="https://github.com/NotLiu">GITHUB</a>
-              </li>
-            </ul>
-          </div>
-          <Routes>
-            <Route exact path="/" element={<About />} />
-
-            <Route path="/work" element={<Work />} />
-
-            <Route path="/projects" element={<Projects />} />
-
-            <Route path="/gallery" element={<Gallery />} />
-
-            <Route path="/inktober" element={<Inktober />} />
-
-            {/* <Route path="/test" element={} /> */}
-          </Routes>
+            <li>
+              <Link to="/projects" id="projects">
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link to="/gallery" id="gallery">
+                GALLERY
+              </Link>
+            </li>
+            <li>
+              <a href="https://github.com/NotLiu">GITHUB</a>
+            </li>
+          </ul>
         </div>
-      </Router>
-    );
-  }
+        <Routes>
+          <Route exact path="/" element={<About />} />
+
+          <Route path="/work" element={<Work />} />
+
+          {/* <Route path="/projects" element={<Projects />} /> */}
+          <Route path="/projects" element={<Projects />} />
+
+          <Route path="/gallery" element={<Gallery />} />
+
+          <Route path="/inktober" element={<Inktober />} />
+
+          {/* <Route path="/test" element={} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
